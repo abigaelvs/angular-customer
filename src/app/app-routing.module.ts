8a98b9ab03d13850c6b621f2bf199817a38ca6b5
abigaelvs <abigaelvas@gmail.com>
2021-11-04 15:19:08 +0700

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { AddCustomerComponent } from './components/add-customer/add-customer.component';
+import { CustomerDetailComponent } from './components/customer-detail/customer-detail.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+
+import { AuthGuard } from './components/auth/auth.guard';
+import { Role } from './models/role';
+
+const routes: Routes = [
+  {path: "", component: HomeComponent, canActivate: [AuthGuard]},
+  {path: "customers", component: CustomersComponent, canActivate: [AuthGuard]},
+  {path: "customers/add", component: AddCustomerComponent, canActivate: [AuthGuard], data: {roles: [Role.Admin, Role.CoAdmin]}},
+  {path: "customers/:id", component: CustomerDetailComponent, canActivate: [AuthGuard], data: {roles: [Role.Admin, Role.CoAdmin]}},
+
+  {path: "login", component: LoginComponent},
+  {path: "signup", component: SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { NgxWebstorageModule } from 'ngx-webstorage';
+import { DataTablesModule } from 'angular-datatables';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './components/home/home.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { CustomerDetailComponent } from './components/customer-detail/customer-detail.component';
+import { AddCustomerComponent } from './components/add-customer/add-customer.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+
+import { InMemoryDataService } from './services/db/in-memory-data.service';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+
+    NavbarComponent,
+    CustomersComponent,
+    CustomerDetailComponent,
+    AddCustomerComponent,
+    LoginComponent,
+    SignupComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    
+    DataTablesModule,
+    NgxWebstorageModule.forRoot(),
+
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {dataEncapsulation: false}
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
